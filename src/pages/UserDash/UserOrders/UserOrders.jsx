ef97cf0a35ba05beee19280020e7553e48ab03cd
Saiful Islam <sakibbhaiya143@gmail.com>
2023-09-15 01:46:49 +0600

    Customer order page added

--- src/pages/UserDash/UserOrders/UserOrders.jsx
+++ src/pages/UserDash/UserOrders/UserOrders.jsx
@@ -4,28 +4,48 @@ import DashHead from '../../../components/DashHead';
 
 const UserOrders = () => {
     const [customerOrders] = useCustomerOrder();
-    console.log(customerOrders);
     return (
        <section>
         <DashHead title="My Orders"/>
 
-<div className='my-20 lg:w-[95%] mx-auto'>
+<div className='my-20 mx-auto'>
 <div className="overflow-x-auto">
   <table className="table">
     {/* head */}
     <thead>
       <tr>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
+        <th>Email</th>
+        <th>Items</th>
+        <th>Base Size</th>
+        <th>Base Price</th>
+        <th>Transaction Id</th>
+        <th>Total</th>
       </tr>
     </thead>
     <tbody>
-      {/* row 1 */}
-      <tr>
+      {
+customerOrders.map(order=><tr key={order._id} className='font-medium'>
+        <td>{order.email}</td>
+
+        <td>
+        {order.products.map(prod=><p>{prod.name}</p>)}
+        </td>
+
+
+        <td>{order.products.map(prod=><p>
+        <span>{prod.baseSize}</span>
+        </p>)}</td>
+
+        <td>
+        {order.products.map(prod=><p>৳ {prod.basePrice} x {prod.quantity}</p>)}
+        </td>
+
+        <td>{order.transactionId}</td>
+        <td>৳ {order.totalPrice}</td>
         
-      </tr>
+    </tr>)
+      }
+
     </tbody>
     
   </table>
